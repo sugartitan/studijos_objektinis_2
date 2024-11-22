e053bcb8d5e3cf8c6dfe0935f81d04c5fd51fca3
Rokas Burneika <bereklamuman@gmail.com>
2024-11-22 19:15:26 +0200

    Changed Struct to Class

--- Include/Student.h
+++ Include/Student.h
@@ -3,10 +3,21 @@
 #include <string>
 #include <vector>
 
-struct Student {
-    std::string name, last_name;
-    std::vector<int> grades;
-    int exam_grade;
+class Student {
+    private:
+        std::string name_;
+        std::string last_name_;
+        std::vector<int> grades_;
+        int exam_grade_;
+
+    public:
+        Student() : name_(""), last_name_(""), grades_(std::vector<int>{}), exam_grade_(0) { }
+        Student(std::string name, std::string last_name, std::vector<int> grades, int exam_grade) : name_(name), last_name_(last_name), grades_(grades), exam_grade_(exam_grade) { };
+        std::string name() const { return name_; }
+        std::string lastName() const { return last_name_; }
+        std::vector<int> getGrades() const { return grades_; }
+        int getExam() const { return exam_grade_; }
+        ~Student() {};
 };
 
 #endif
