71384f8f3570f932b740824225f957fe6ecc3afc
Rokas Burneika <bereklamuman@gmail.com>
2024-12-13 13:27:32 +0200

    Corrected destructor

--- Include/Human.h
+++ Include/Human.h
@@ -12,7 +12,7 @@ class Human {
         Human(std::string name, std::string last_name) : name_(name), last_name_(last_name) { }
         std::string name() const { return name_; }
         std::string lastName() const { return last_name_; }
-        virtual ~Human() = 0;
+        virtual ~Human() {};
 };
 
 #endif
