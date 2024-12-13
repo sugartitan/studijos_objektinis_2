338f2dc974c63013f92af08d1bb1ee224082efd3
Rokas Burneika <bereklamuman@gmail.com>
2024-12-13 12:03:53 +0200

    Added doxygen

---
+++ doxygen/search/all_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['1_0',['1',['../md__r_e_a_d_m_e.html#autotoc_md3',1,'V0.1'],['../md__r_e_a_d_m_e.html#autotoc_md4',1,'V0.1.1'],['../md__r_e_a_d_m_e.html#autotoc_md10',1,'V1.1']]],
+  ['1_201_1',['V0.1.1',['../md__r_e_a_d_m_e.html#autotoc_md4',1,'']]]
+];
---
+++ doxygen/search/all_9.js
@@ -0,0 +1,11 @@
+var searchData=
+[
+  ['v0_201_0',['V0.1',['../md__r_e_a_d_m_e.html#autotoc_md3',1,'']]],
+  ['v0_201_201_1',['V0.1.1',['../md__r_e_a_d_m_e.html#autotoc_md4',1,'']]],
+  ['v0_202_2',['V0.2',['../md__r_e_a_d_m_e.html#autotoc_md5',1,'']]],
+  ['v0_203_3',['V0.3',['../md__r_e_a_d_m_e.html#autotoc_md6',1,'']]],
+  ['v1_200_4',['V1.0',['../md__r_e_a_d_m_e.html#autotoc_md8',1,'']]],
+  ['v1_201_5',['V1.1',['../md__r_e_a_d_m_e.html#autotoc_md10',1,'']]],
+  ['v1_202_6',['V1.2',['../md__r_e_a_d_m_e.html#autotoc_md12',1,'']]],
+  ['v1_203_7',['V1.3',['../md__r_e_a_d_m_e.html#autotoc_md13',1,'']]]
+];
